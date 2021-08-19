952ad30df32e05194a4066bb132edda215b1f301
thayanebatista <thayane.batista.ec@gmail.com>
2021-08-19 00:36:25 -0400

    n

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.css'
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
